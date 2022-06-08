3c5582eb35086601b377031ad927f6080e3950bc
subhradwip01 <subhradwip01@gmail.com>
2022-06-08 22:14:04 +0530

    Widget lg done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/components/WidgetLg/WidgetLg.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import "./WidgetLg.css"
+const WidgetLg = () => {
+  return (
+    <div className='widgetLg'>
+      <h3 className='widgetLgTitle'>Latest Movies</h3>
+      <table className='widgetLgTable'>
+        
+          <tr className="widgetLgTr">
+            <th className="widgetLgTh">Name</th>
+            <th className="widgetLgTh">Release Date</th>
+            <th className="widgetLgTh">Genere</th>
+            <th className="widgetLgTh">Age Limit</th>
+          </tr>
+          <tr className="widgetLgTr">
+            <td className="widgetLgTdName">Name 1</td>
+            <td className="widgetLgTdDate">Release Date 1</td>
+            <td className="widgetLgTdGenre">Genere 2</td>
+            <td className="widgetLgTdAge">16+</td>
+          </tr>
+          <tr className="widgetLgTr">
+            <td className="widgetLgTdName">Name 1</td>
+            <td className="widgetLgTdDate">Release Date 1</td>
+            <td className="widgetLgTdGenre">Genere 2</td>
+            <td className="widgetLgTdAge">16+</td>
+          </tr>
+          <tr className="widgetLgTr">
+            <td className="widgetLgTdName">Name 1</td>
+            <td className="widgetLgTdDate">Release Date 1</td>
+            <td className="widgetLgTdGenre">Genere 2</td>
+            <td className="widgetLgTdAge">16+</td>
+          </tr>
+          <tr className="widgetLgTr">
+            <td className="widgetLgTdName">Name 1</td>
+            <td className="widgetLgTdDate">Release Date 1</td>
+            <td className="widgetLgTdGenre">Genere 2</td>
+            <td className="widgetLgTdAge">16+</td>
+          </tr>
+          <tr className="widgetLgTr">
+            <td className="widgetLgTdName">Name 1</td>
+            <td className="widgetLgTdDate">Release Date 1</td>
+            <td className="widgetLgTdGenre">Genere 2</td>
+            <td className="widgetLgTdAge">16+</td>
+          </tr>
+          <tr className="widgetLgTr">
+            <td className="widgetLgTdName">Name 1</td>
+            <td className="widgetLgTdDate">Release Date 1</td>
+            <td className="widgetLgTdGenre">Genere 2</td>
+            <td className="widgetLgTdAge">16+</td>
+          </tr>
+         
+      
+      </table>
+    </div>
+  )
+}
+
+export default WidgetLg
