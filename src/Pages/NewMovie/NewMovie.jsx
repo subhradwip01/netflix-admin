07b5dc771ca5b7d72a850bf6605ab381b8d1359b
subhradwip01 <subhradwip01@gmail.com>
2022-06-15 13:34:05 +0530

    Ne movie page done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/Pages/NewMovie/NewMovie.jsx
@@ -0,0 +1,141 @@
+import React,{useState} from 'react'
+import "./NewMovie.css"
+
+const NewMovie = () => {
+    const [movie, setMovie] = useState(null);
+  const [img, setImg] = useState(null);
+  const [imgTitle, setImgTitle] = useState(null);
+  const [imgSm, setImgSm] = useState(null);
+  const [trailer, setTrailer] = useState(null);
+  const [video, setVideo] = useState(null);
+  const [uploaded, setUploaded] = useState(0);
+  
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setMovie({ ...movie, [e.target.name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Movie is Uploaded ",movie)
+  };
+
+  return (
+    <div className="newMovie">
+      <h1 className="addMovieTitle">New Movie</h1>
+      <form className="addMovieForm">
+        <div className="addMovieItem">
+          <label>Image</label>
+          <input
+            type="file"
+            id="img"
+            name="img"
+            onChange={(e) => setImg(e.target.files[0])}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Title image</label>
+          <input
+            type="file"
+            id="imgTitle"
+            name="imgTitle"
+            onChange={(e) => setImgTitle(e.target.files[0])}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Thumbnail image</label>
+          <input
+            type="file"
+            id="imgSm"
+            name="imgSm"
+            onChange={(e) => setImgSm(e.target.files[0])}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="John Wick"
+            name="title"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Description</label>
+          <input
+            type="text"
+            placeholder="description"
+            name="desc"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Year</label>
+          <input
+            type="text"
+            placeholder="Year"
+            name="year"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Genre</label>
+          <input
+            type="text"
+            placeholder="Genre"
+            name="genre"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Duration</label>
+          <input
+            type="text"
+            placeholder="Duration"
+            name="duration"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Limit</label>
+          <input
+            type="text"
+            placeholder="limit"
+            name="limit"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Is Series?</label>
+          <select name="isSeries" id="isSeries" onChange={handleChange}>
+            <option value="false">No</option>
+            <option value="true">Yes</option>
+          </select>
+        </div>
+        <div className="addMovieItem">
+          <label>Trailer</label>
+          <input
+            type="file"
+            name="trailer"
+            onChange={(e) => setTrailer(e.target.files[0])}
+          />
+        </div>
+        <div className="addMovieItem">
+          <label>Video</label>
+          <input
+            type="file"
+            name="video"
+            onChange={(e) => setVideo(e.target.files[0])}
+          />
+        </div>
+        
+          <button className="addMovieButton" onClick={handleSubmit}>
+            Create
+          </button>
+        
+      </form>
+    </div>
+  )
+}
+
+export default NewMovie
