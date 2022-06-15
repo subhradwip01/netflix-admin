7e1f80b9fc54b0c9bfc3808ff5d673ab05d8329f
subhradwip01 <subhradwip01@gmail.com>
2022-06-15 13:48:39 +0530

    Refactoring users and movies page
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import "./Movies.css";
-import { Delete, Edit } from "@material-ui/icons"
-import { Link } from 'react-router-dom'
-import { DataGrid } from '@material-ui/data-grid';
+import DataTable from '../../components/DataTable/DataTable';
+
 const rows = [
     { id: 1, img: "https://www.cheatsheet.com/wp-content/uploads/2019/12/money-heist-banner.png", title: "Money Heist", genre: "Thriller", year: 2020, limit: 16, isSeries: "True" },
     { id: 2, img: "https://www.cheatsheet.com/wp-content/uploads/2019/12/money-heist-banner.png", title: "Money Heist", genre: "Thriller", year: 2020, limit: 16, isSeries: "True" },
@@ -20,44 +19,10 @@ const Movies = () => {
         let newD = data.filter(d => d.id !== id);
         setData(newD)
     }
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        {
-            field: 'movie', headerName: 'Movie', width: 200, renderCell: (params) => {
-                return (
-                    <div className='movieTitle'>
-                        <img src={params.row.img} alt="" srcset="" />{params.row.title}
-                    </div>
-                )
-            }
-        },
-        { field: "genre", headerName: "Genre", width: 120 },
-        { field: "year", headerName: "Year", width: 120 },
-        { field: "limit", headerName: "Limit", width: 120 },
-        { field: "isSeries", headerName: "isSeries", width: 120 },
-        {
-            field: "action", headerName: "Action", width: 150, renderCell: params => {
-               return( <>
-                    <Link to={`/movies/${params.row.id}`}>
-                        <button className="moviesEdit"><Edit />Edit</button>
-                    </Link>
-                    <Delete className="moviesDelete" onClick={() => deleteHandler(params.row.id)} />
-                </>
-               )
-            }
-        }
-
-    ];
+    
     return (
         <div className="movies">
-            <DataGrid
-                rows={data}
-                columns={columns}
-                pageSize={10}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-                disableSelectionOnClick
-            />
+            <DataTable onDelete={deleteHandler} type="movie" data={data}/>
         </div>
     )
 }
--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,7 +1,5 @@
 import React,{useState} from 'react'
-import { DataGrid } from '@material-ui/data-grid'
-import {Delete,Edit} from "@material-ui/icons"
-import { Link } from 'react-router-dom'
+import DataTable from '../../components/DataTable/DataTable'
 import "./Users.css"
 const rows = [
     { id: 1,avatar:"https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500", userName: 'Snow',email:"xyz@gmail.com" },
@@ -20,48 +18,9 @@ const Users = () => {
         setData(newD)
     }
     
-    
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'user', headerName: 'Username', width: 230 ,renderCell:(params)=>{
-            return (
-                <div className='userTitle'>
-                    <img src={params.row.avatar} alt="" srcset="" />{params.row.userName}
-                </div>
-            )
-        }},
-        { field: 'email', headerName: 'Email', width: 200 },
-        {
-            field:"action",
-            headerName:"Acttion",
-            width:170,
-            renderCell:params=>{
-               return (
-                <>
-                <Link to={`${params.row.id}`}>
-                    <button className="usersEdit"><Edit/>Edit</button>
-                </Link>
-                <Delete  className='usersDelete' onClick={()=>deleteHandler(params.row.id)}/>
-                </>
-               ) 
-            }
-        }
-        
-    ];
-
-
-    
-
     return (
         <div className='users'>
-            <DataGrid 
-            rows={data}
-            columns={columns}
-            pageSize={10}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            disableSelectionOnClick
-            />
+            <DataTable onDelete={deleteHandler} type="movie" data={data}/>
         </div>
     )
 }
