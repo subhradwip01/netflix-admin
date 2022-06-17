c05aa79abb1e061f8b594f691c57cb4ccec60f7b
subhradwip01 <subhradwip01@gmail.com>
2022-06-17 22:12:58 +0530

    API data fetching in home page
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,41 +1,88 @@
-import React from 'react'
-import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo';
-import Chart from '../../components/Chart/Chart';
-import "./Home.css"
-import WidgetLg from '../../components/WidgetLg/WidgetLg';
-import WidgetSm from '../../components/WidgetSm/WidgetSm';
+import React, { useState, useEffect, useMemo } from "react";
+import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import Chart from "../../components/Chart/Chart";
+import "./Home.css";
+import WidgetLg from "../../components/WidgetLg/WidgetLg";
+import WidgetSm from "../../components/WidgetSm/WidgetSm";
+import axios from "axios";
 const Home = () => {
-  const data = [
-    {
-        name: 'Jan',
-        "Active User": 4000,
-    },
-    {
-        name: 'Feb',
-        "Active User": 3000,
-    },
-    {
-        name: 'March',
-        "Active User": 2000,
+  const months = useMemo(
+    () => [
+      "Januray",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
 
-    },
-    {
-        name: 'April',
-        "Active User": 2780,
-    },
+    []
+  );
 
-];
+  const [userStatData, setUserStatData] = useState([]);
+  const [userStatError, setUserStateError] = useState(false);
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get("/users/stats", {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNzJhMGYwOTY2YTNkMDdlNjlmZjk5MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NTQ2OTg3OCwiZXhwIjoxNjU1NTU2Mjc4fQ.w5hP5O3tML8A9TCbDwetZYg2u-A8e2u-rfoZSLE8KQU",
+          },
+        });
+        if (res.status === 403) {
+          window.alert("You are not adim to see data");
+          setUserStateError(true);
+          return;
+        }
+        if (res.status !== 200) {
+          setUserStateError(true);
+          return;
+        }
+        setUserStateError(false);
+        let userStat = [];
+        res.data.map((item) =>
+          userStat.push({
+            name: months[item._id - 1],
+            "Active User": item.total,
+          })
+        );
+        setUserStatData(userStat);
+      } catch (error) {
+        setUserStateError(true);
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  console.log(userStatData);
   return (
-    <div className='home'>
-        <FeaturedInfo />
-        <Chart title="User Analytics" data={data} grid dataKey="Active User"/>
-        <div className="homeWidgets">
-          <WidgetSm/>
-          <WidgetLg/>
-        </div>
+    <div className="home">
+      <FeaturedInfo />
+      {!userStatError ? (
+        <Chart
+          title="User Analytics"
+          data={userStatData}
+          grid
+          dataKey="Active User"
+        />
+      ) : (
+        <h3 style={{ padding: "20px" }}>Sorry! Unable to fetch User stat</h3>
+      )}
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/WidgetSm/WidgetSm.jsx
+++ src/components/WidgetSm/WidgetSm.jsx
@@ -1,79 +1,85 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./WidgetSm.css";
-import { Email,Person } from "@material-ui/icons";
+import { Email, Person } from "@material-ui/icons";
 
 export default function WidgetSm() {
+  const [latestUser, setLatestUser] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getLatestUser = async () => {
+      try {
+        const res = await axios.get("/users?latest=true", {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNzJhMGYwOTY2YTNkMDdlNjlmZjk5MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NTQ2OTg3OCwiZXhwIjoxNjU1NTU2Mjc4fQ.w5hP5O3tML8A9TCbDwetZYg2u-A8e2u-rfoZSLE8KQU",
+          },
+        });
+        if (res.status !== 200) {
+          setError(true);
+          return;
+        }
+        console.log(res.data.users);
+        let users = [];
+        res.data.users.map((user) =>
+          users.push({
+            profilePic: user.profilePic,
+            email: user.email,
+            username: user.username,
+          })
+        );
+        setError(false);
+        setLatestUser(users);
+      } catch (error) {
+        setError(true);
+        console.log(error);
+      }
+    };
+    getLatestUser();
+  }, []);
+
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
-      <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername"><Person className="widgetSmIcon"/>Anna Keller</span>
-            <span className="widgetSmUserEmail"><Email className="widgetSmIcon"/>Software Engineer</span>
-          </div>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername"><Person className="widgetSmIcon"/>Anna Keller</span>
-            <span className="widgetSmUserEmail"><Email className="widgetSmIcon"/>Software Engineer</span>
-          </div>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername"><Person className="widgetSmIcon"/>Anna Keller</span>
-            <span className="widgetSmUserEmail"><Email className="widgetSmIcon"/>Software Engineer</span>
-          </div>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername"><Person className="widgetSmIcon"/>Anna Keller</span>
-            <span className="widgetSmUserEmail"><Email className="widgetSmIcon"/>Software Engineer</span>
-          </div>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername"><Person className="widgetSmIcon"/>Anna Keller</span>
-            <span className="widgetSmUserEmail"><Email className="widgetSmIcon"/>Software Engineer</span>
-          </div>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername"><Person className="widgetSmIcon"/>Anna Keller</span>
-            <span className="widgetSmUserEmail"><Email className="widgetSmIcon"/>Software Engineer</span>
-          </div>
-        </li>
-       
-      </ul>
+      {!error ? (
+        <ul className="widgetSmList">
+          {latestUser.map(({ profilePic, username, email }) => (
+            <UserItem
+              profilePic={profilePic}
+              username={username}
+              email={email}
+            />
+          ))}
+        </ul>
+      ):
+      <h3>Sorry! Unable to fetch latest user</h3>}
     </div>
   );
 }
+
+const UserItem = ({ profilePic, username, email }) => {
+  return (
+    <li className="widgetSmListItem">
+      <img
+        src={
+          profilePic
+            ? profilePic
+            : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+        }
+        alt=""
+        className="widgetSmImg"
+      />
+      <div className="widgetSmUser">
+        <span className="widgetSmUsername">
+          <Person className="widgetSmIcon" />
+          {username}
+        </span>
+        <span className="widgetSmUserEmail">
+          <Email className="widgetSmIcon" />
+          {email}
+        </span>
+      </div>
+    </li>
+  );
+};
