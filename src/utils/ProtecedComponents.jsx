e398de80abdb20014fafb22a31d4dd3fa90251c7
subhradwip01 <subhradwip01@gmail.com>
2022-06-21 14:03:58 +0530

    Protected routes created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/utils/ProtecedComponents.jsx
@@ -0,0 +1,17 @@
+
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { AuthContext } from '../context/authContext/AuthContext'
+
+const ProtecedComponents = ({Component}) => {
+  const {user}= useContext(AuthContext)
+  if(!user){
+    return <Navigate to="login" replace/>
+  }
+
+  
+
+  return Component
+}
+
+export default ProtecedComponents
