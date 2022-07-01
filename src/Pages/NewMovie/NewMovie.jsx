b23bd72e20cf55126170e5e5deaa57e44511ef49
subhradwip01 <subhradwip01@gmail.com>
2022-07-01 12:55:53 +0530

    New movie done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/Pages/NewMovie/NewMovie.jsx
+++ src/Pages/NewMovie/NewMovie.jsx
@@ -1,23 +1,68 @@
-import React,{useState} from 'react'
-import "./NewMovie.css"
-
+import React, { useState } from "react";
+import "./NewMovie.css";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import storage from "../../firebase";
+import { createMovie } from "../../context/movieContext/apiCalls";
+import { useContext } from "react";
+import { MovieContext } from "../../context/movieContext/MovieContetxt";
+import { AuthContext } from "../../context/authContext/AuthContext";
 const NewMovie = () => {
-    const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState(null);
   const [img, setImg] = useState(null);
   const [imgTitle, setImgTitle] = useState(null);
   const [imgSm, setImgSm] = useState(null);
   const [trailer, setTrailer] = useState(null);
   const [video, setVideo] = useState(null);
   const [uploaded, setUploaded] = useState(0);
-  
+  const {dispatch}=useContext(MovieContext)
+  const {user}=useContext(AuthContext)
+
   const handleChange = (e) => {
     const value = e.target.value;
     setMovie({ ...movie, [e.target.name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const upload = (items) => {
+    items.forEach((item) => {
+      const filename=new Date().getTime() + item.file.name
+      const bucketRef = ref(storage, `items/${filename}`);
+      const uploadTask = uploadBytesResumable(bucketRef, item.file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            setMovie((prev) => ({ ...prev, [item.label]: url }));
+            setUploaded(prev=>prev+1)
+          });
+        }
+      );
+    });
+  };
+
+  const handleUpload = (e) => {
     e.preventDefault();
-    console.log("Movie is Uploaded ",movie)
+    upload([
+      { file: img, label: "imgBanner" },
+      { file: imgTitle, label: "imgTitle" },
+      { file: imgSm, label: "imgThumbnail" },
+      { file: trailer, label: "trailer" },
+      { file: video, label: "video" },
+    ]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Movie is Uploaded ", movie);
+    await createMovie(dispatch,movie,user.token);
+    alert("Movie added:  ",movie)
   };
 
   return (
@@ -128,14 +173,18 @@ const NewMovie = () => {
             onChange={(e) => setVideo(e.target.files[0])}
           />
         </div>
-        
+        {uploaded === 5 ? (
           <button className="addMovieButton" onClick={handleSubmit}>
             Create
           </button>
-        
+        ) : (
+          <button className="addMovieButton" onClick={handleUpload}>
+            Upalod
+          </button>
+        )}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewMovie
+export default NewMovie;
